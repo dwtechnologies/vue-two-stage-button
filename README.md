efa4bde93bddcfd5c3c036607514fddc263c8a44
Alessio Maffeis <alessio.maffeis@danielwellington.com>
2017-11-10 14:37:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+vue-two-stage-button
+=============
+
+Two Stage Button Component
+
+
+
+## Installation
+
+### npm
+
+``` sh
+npm install --save vue-picture-input
+```
+### yarn
+
+``` sh
+yarn add vue-picture-input
+```
+
+## Usage
+
+
+## Example project
+
+
+## Props
+
+   
+## Events
+
+
+## TODOs
+
+
+## Contributions
+
+All contributions are welcome, as long as they are within the scope of the project.
+
+Please follow the Javascript Standard Style guidelines:
+https://standardjs.com
---
+++ TwoStageButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button :class="classes">
+    <slot v-if="!engaged" name="stage-1"></slot>
+    <slot v-else name="stage-2"></slot>
+  </button>
+</template>
+
+<script>
+
+export default {
+  name: 'two-stage-button',
+  props: {
+    classes: {
+      type: String,
+      default: 'button two-stage-button'
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      engaged: false
+    }
+  },
+  methods: {
+    onClick () {
+
+    },
+    onBlur () {
+
+    }
+  }
+}
+</script>
